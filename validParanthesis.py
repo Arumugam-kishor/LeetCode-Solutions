7c2f4b92cba2b8036778bdd44d15f894fd8d1ea5
Arumugam-kishor <131270825+Arumugam-kishor@users.noreply.github.com>
2023-09-27 18:57:45 +0530

    Update validParanthesis.py

--- validParanthesis.py
+++ validParanthesis.py
@@ -1,32 +1,15 @@
-s="([)]"
-arr=[]
-stack=[]
-top=0
-for i in s:
-    arr.append(ord(i))
-for i in arr:
-    if(i==40 or i==91 or i==123):
-        stack.append(i)
-        top+=1
-    if(i==41):
-        arr1=arr[0:arr.index(i)]
-        for j in arr1:
-            if(j==40):
-                stack.remove(40)
-                top-=1
-    if(i==93):
-        arr2=arr[0:arr.index(i)]
-        for j in arr2:
-            if(j==91):
-                stack.remove(91)
-                top-=1
-    if(i==125):
-        arr3=arr[0:arr.index(i)]
-        for j in arr3:
-            if(j==123):
-                stack.remove(123)
-                top-=1
-if(top==0):
-    print('true')
-else:
-    print('false')
+class Solution(object):
+    def isValid(self, s):
+        ascii=[ord(i) for i in s]
+        stack=[]
+        for i in range(0,len(ascii)):
+            stack.append(ascii[i])
+            length=len(stack)
+            if len(stack)>=2:
+                if stack[len(stack)-1]-stack[len(stack)-2]==1 or stack[len(stack)-1]-stack[len(stack)-2]==2:
+                    stack.pop(length-1)
+                    stack.pop(length-2)
+        if len(stack)==0:
+            return True
+        else:
+            return False
