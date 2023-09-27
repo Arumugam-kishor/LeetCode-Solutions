03dd2eb9d5ecadb6b278d78fdf04ffddc2b47a8b
Arumugam-kishor <131270825+Arumugam-kishor@users.noreply.github.com>
2023-09-27 19:07:02 +0530

    Delete powerOfTwo.py

--- powerOfTwo.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def isPowerOfTwo(self, n):
-        import math
-        for i in range(0,33):
-            if(n==math.pow(2,i)):
-                return True
-        return False
