08907c8dc9361ea55e79c9ae71df80db9b937259
Arumugam-kishor <131270825+Arumugam-kishor@users.noreply.github.com>
2023-09-27 18:54:33 +0530

    Update plusone.py

--- plusone.py
+++ plusone.py
@@ -1,11 +1,14 @@
-digits=[9,9,9,9]
-n=len(digits)
-digits[n-1]+=1
-for i in range (len(digits)-1,0,-1):
-    if digits[i]==10:
-        digits[i]=0
-        digits[i-1]+=1
-if(digits[0]==10):
-    digits.insert(0,1)
-    digits[1]=0
-print(digits)
+class Solution(object):
+    def plusOne(self, digits):
+        newf=[]
+        c=0
+        for i in digits:
+            c=c*10+i
+        c=c+1
+        print(c)
+        while(c!=0):
+            d=c%10
+            c=c//10
+            newf.append(d)
+        newf.reverse()
+        return newf        
