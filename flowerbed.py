77cb1fc0aa2f8adf44fb816d61638a3232961491
Arumugam-kishor <131270825+Arumugam-kishor@users.noreply.github.com>
2023-09-27 18:51:25 +0530

    Update flowerbed.py

--- flowerbed.py
+++ flowerbed.py
@@ -1,17 +1,15 @@
-flowerbed=[1,0,0,0,0,1]
-n=2
-flowerbed.insert(0,0)
-flowerbed.append(0)
-i=1
-while True:
-    if(n==0):
-        print("True")
-        break
-    if(i>len(flowerbed)-2):
-        print("False")
-        break
-    if(flowerbed[i]==0 and flowerbed[i+1]==0 and flowerbed[i-1]==0):
-        n-=1
-        i+=2
-    else:
-        i+=1    
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        flowerbed.insert(0,0)
+        flowerbed.append(0)
+        i=1
+        while True:
+            if(n==0):
+                return True
+            if(i>len(flowerbed)-2):
+                return False 
+            if(flowerbed[i]==0 and flowerbed[i+1]==0 and flowerbed[i-1]==0):
+                n-=1
+                i+=2
+            else:
+                i+=1                   
