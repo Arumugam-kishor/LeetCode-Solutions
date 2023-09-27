7f8711d26089c45b2eee96c9cb9bcbc18b8e395a
Arumugam-kishor <131270825+Arumugam-kishor@users.noreply.github.com>
2023-09-27 18:55:54 +0530

    Update powerOfTwo.py

--- powerOfTwo.py
+++ powerOfTwo.py
@@ -1,10 +1,7 @@
-import math
-n=33
-key=0
-for i in range(0,33):
-    if(n==math.pow(2,i)):
-        key=1
-if(key==1):
-    print(True)
-else:
-    print(False)
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        import math
+        for i in range(0,33):
+            if(n==math.pow(2,i)):
+                return True
+        return False
