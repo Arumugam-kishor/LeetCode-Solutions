2f556b8f3b72953b0570acdf8accd28bf791abab
Arumugam-kishor <131270825+Arumugam-kishor@users.noreply.github.com>
2023-09-27 18:48:26 +0530

    Update containerWithMostWater.py

--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -1,17 +1,15 @@
-height=[1,8,6,2,5,4,8,3,7]
-left = 0
-right = len(height) - 1
-max_area = 0
-
-while left < right:
-    width = right - left
-    h = min(height[left], height[right])
-    area = width * h
-    max_area = max(max_area, area)
-    
-    if height[left] < height[right]:
-        left += 1
-    else:
-        right -= 1
-
-print (max_area)
+class Solution(object):
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        while left < right:
+            width = right - left
+            h = min(height[left], height[right])
+            area = width * h
+            max_area = max(max_area, area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
